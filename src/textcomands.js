e09317b209c9a04ca28c57fd9905a23d7c9c597d
* <luiz.felipe.u38@gmail.com>
2021-03-20 20:53:53 -0300

    First commit

---
+++ src/textcomands.js
@@ -0,0 +1,15 @@
+const textcomands = (p) => {
+return `sєנαм вєм-viท∂σs ασ мєทυ ∂σ
+💎 ▁▂▃༻ƒєℓiρiท-вστ v1.0༺▃▂▁ 💎
+╔════ ❯ 𝓬𝓶𝓭 𝓭𝓮 𝓽𝓮𝔁𝓽𝓸 ❮ ══════════
+║ ❯ *${p}glitch* <text1> | <text2>
+║ ❯ *${p}ravetext* <text1> | <text2>
+║ ❯ *${p}woodtext* <text1> | <text2>
+║ ❯ *${p}neon* <text1> | <text2>
+║ ❯ *${p}sunset* <text1> | <text2>
+║ ❯ *${p}gimage* <text>
+║ ❯ *${p}whatis* <text>
+║ ❯ *${p}text3d* <text>
+╚═══════════════════════════`
+}
+exports.textcomands = textcomands
---
+++ src/welcomtext.js
@@ -0,0 +1,9 @@
+const welcome = (number, groupname) => {
+    return `ᴏᴘᴀ @${number}. sᴇᴊᴀ ʙᴇᴍ-ᴠɪɴᴅᴏ ᴀᴏ ɢʀᴜᴘᴏ ${groupname}`
+}
+exports.welcome = welcome
+
+const bye = (number) => {
+    return `Adeus @${number}. ᴅᴇɪᴛᴏ ᴄᴏᴍᴘʟᴇx 🛌
+}
+exports.bye = bye
